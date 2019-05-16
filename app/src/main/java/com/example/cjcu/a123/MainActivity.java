c14ed8074b555791629f41320a83c29186d85734
yuki121131 <da40825@gmail.com>
2019-05-16 14:52:23 +0800

    add spinner function

--- app/src/main/java/com/example/cjcu/a123/MainActivity.java
+++ app/src/main/java/com/example/cjcu/a123/MainActivity.java
@@ -2,17 +2,43 @@ package com.example.cjcu.a123;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner sp_fruit;
+    private Button button;
+    private EditText ed_fruit;
+
+    String[] fruit = {"apple","orange","banana"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        Spinner spinner = findViewById(R.id.sp_fruit);
+        button = findViewById(R.id.button);
+        ed_fruit = findViewById(R.id.ed_fruit);
+        sp_fruit = findViewById(R.id.sp_fruit);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,fruit);
+        sp_fruit.setAdapter(adapter);
+
+        sp_fruit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ed_fruit.setText(sp_fruit.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
 }
